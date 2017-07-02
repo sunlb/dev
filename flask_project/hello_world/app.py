29101870f40f92a4fbe29e4b70777c24ae44b7ce
sunlb <sunlb@me.com>
2017-07-02 12:59:32 +0800

    for branch test1

--- flask_project/hello_world/app.py
+++ flask_project/hello_world/app.py
@@ -1,10 +1,11 @@
-from flask import Flask 
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
     return "Hello"
 
+# ...
 if __name__ == "__main__":
     app.run()
 
