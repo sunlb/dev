d03d4aa1c7b337840708db8660aa5a2e2e72724f
sunlb <sunlb@me.com>
2017-07-10 00:08:02 +0800

    create helloworld2, testing flask get/post

---
+++ flask_project/hello_world2/app.py
@@ -0,0 +1,32 @@
+import sys, os, logging, json
+from flask import Flask, render_template, request, current_app, g
+app = Flask(__name__)
+
+# comment...
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+@app.route("/api/get/<id>", methods=["GET"])
+def api_get(id):
+    print "request.args:", request.args
+    print "request.get_data()", request.get_data()
+    print dir(request)
+    data = {"id": id}
+    return json.dumps(data)
+
+@app.route("/api/post", methods=["POST"])
+def api_post():
+    print "request.get_data()", request.get_data()
+    print "request.get_json()", request.get_json()
+    print "request.data", request.data
+    print "request.form", request.form
+    data = json.loads(request.data)
+    print "dict:", data
+    return '''{"message": "hello"}'''
+
+# ...
+if __name__ == "__main__":
+    app.run()
+
+
